68b6c79c7ed5ef7a541a411ea0925c93b2e47bdb
damjack <giacomello.damiano@gmail.com>
2018-09-04 17:30:14 +0200

    Add base assets

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// Bootstrap dependencies
+window.$ = window.jQuery = require('jquery') // required for bootstrap
+window.Popper = require('popper.js') // required for tooltip, popup...
+require('bootstrap')
+
+
