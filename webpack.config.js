b000485361aa399e7b58a666df2641903b7a1283
damjack <giacomello.damiano@gmail.com>
2018-09-04 09:35:33 +0200

    Init repo

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "css/[name].css",
+  disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: {
+    application: './assets/js/index.js',
+    styles: './assets/scss/application.sass'
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'assets/js'),
+      "node_modules"
+    ],
+    alias: {
+      modernizr$: path.resolve(__dirname, ".modernizrrc.js"),
+      'slick': 'slick-carousel/slick/slick'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, './public/assets'),
+    filename: 'js/[name].js',
+    publicPath: '/assets',
+  },
+  module: {
+    rules: [
+      {
+        loader: "webpack-modernizr-loader",
+        test: /\.modernizrrc\.js$/
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        enforce: 'pre',
+        test: /\.s[ac]ss/,
+        use: 'import-glob-loader'
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: extractSass.extract({
+          use: [
+            { loader: "css-loader" },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [autoprefixer()]
+              }
+            },
+            { loader: "sass-loader" }
+          ],
+          fallback: "style-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      comments: false,
+    })
+  ]
+};
