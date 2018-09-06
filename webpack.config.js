b07aa3dab0ed091390d7e55b1a4e8f3db6d51ac5
Lorezz <point.point@gmail.com>
2018-09-06 14:29:38 +0200

    webpack conf x font and immages + iconfont bs italia

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,34 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 
 const extractSass = new ExtractTextPlugin({
   filename: "css/[name].css",
-  disable: process.env.NODE_ENV === "development"
-})
+});
 
 module.exports = {
   entry: {
-    application: './assets/js/index.js',
-    styles: './assets/sass/application.sass'
+    application: "./assets/js/index.js",
+    styles: "./assets/sass/application.sass"
   },
   resolve: {
-    modules: [
-      path.join(__dirname, 'assets/js'),
-      "node_modules"
-    ],
+    modules: [path.join(__dirname, "assets/js"), "node_modules"],
+    extensions: [".js", ".jsx"],
     alias: {
       modernizr$: path.resolve(__dirname, ".modernizrrc.js")
     }
   },
   output: {
-    path: path.resolve(__dirname, (process.env.NODE_ENV === "development") ? './src/assets' : '../build/assets'),
-    filename: 'js/[name].js',
-    publicPath: '/assets'
+    path: path.resolve(
+      __dirname,
+      process.env.NODE_ENV === "development"
+        ? "./src/assets"
+        : "../build/assets"
+    ),
+    filename: "js/[name].js",
+    publicPath: "/assets/"
   },
   optimization: {
     minimizer: [
@@ -34,7 +36,7 @@ module.exports = {
         uglifyOptions: {
           warnings: false,
           compress: true,
-          comments: false,
+          comments: false
         }
       })
     ]
@@ -49,24 +51,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
-        enforce: 'pre',
-        test: /\.s[ac]ss/,
-        use: 'import-glob-loader'
+        enforce: "pre",
+        test: /\.s?css/,
+        use: "import-glob-loader"
       },
       {
-        test: /\.s[ac]ss$/,
+        test: /\.s?[ac]ss$/,
         use: extractSass.extract({
           use: [
             { loader: "css-loader" },
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
                 plugins: () => [autoprefixer()]
               }
@@ -75,20 +77,28 @@ module.exports = {
           ],
           fallback: "style-loader"
         })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: ["file-loader"]
       }
     ]
   },
   plugins: [
     extractSass,
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     }),
     new webpack.ProvidePlugin({
       $: "jquery", // Used for Bootstrap JavaScript components
       jQuery: "jquery", // Used for Bootstrap JavaScript components
-      Popper: ['popper.js', 'default'] // Used for Bootstrap dropdown, popup and tooltip JavaScript components
+      Popper: ["popper.js", "default"] // Used for Bootstrap dropdown, popup and tooltip JavaScript components
     })
   ]
 };
