eb190b44c019d92073fa21293cfbafe7319c52e7
damjack <giacomello.damiano@gmail.com>
2018-09-14 15:14:48 +0200

    Update webpack and default layout assets

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,17 @@ module.exports = {
           comments: false
         }
       })
-    ]
+    ],
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
   },
   module: {
     rules: [
